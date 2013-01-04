51cb3a3559614a4d344b69ef748e4ba99f338dfe
Sean Zicari <sean.zicari@gmail.com>
2013-01-04 00:00:01 -0800

    Learning how to use the panels lib.

--- panel_browsing.c
+++ panel_browsing.c
@@ -1,9 +1,13 @@
 #include <panel.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define NLINES 10
 #define NCOLS 40
 
-void init_wins(WINDOW * wins[], int n);
+void init_wins(WINDOW * wins[], size_t n);
 void win_show(WINDOW * win, char * label, int label_color);
 void print_in_middle(WINDOW * win, int starty, int startx, int width, char * string, chtype color);
 
@@ -12,12 +16,116 @@ int main(int argc, char * argv[])
     WINDOW * my_wins[3];
     PANEL  * my_panels[3];
     PANEL  * top;
-    int      ch;
+    int      ch, i;
+    size_t   my_wins_size = sizeof my_wins / sizeof(my_wins[0]);
 
     /* Initialize curses */
     initscr();
     start_color();
     cbreak();
     noecho();
-    keypad();
+    keypad(stdscr, true);
+
+    /* Initialize all the colors */
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_BLUE, COLOR_BLACK);
+    init_pair(4, COLOR_CYAN, COLOR_BLACK);
+
+    init_wins(my_wins, my_wins_size);
+
+    /* Attach a panel to each window */
+    for (i = 0; i < my_wins_size; i++)
+        my_panels[i] = new_panel(my_wins[i]);
+
+    /* Set up the user pointers to the next panel */
+    set_panel_userptr(my_panels[0], my_panels[1]);
+    set_panel_userptr(my_panels[1], my_panels[2]);
+    set_panel_userptr(my_panels[2], my_panels[0]);
+
+    /* Update the stacking order. 2nd panel will be on top */
+    update_panels();
+
+    /* Show it on the screen */
+    attron(COLOR_PAIR(4));
+    mvprintw(LINES - 2, 0, "Use tab to browse through the windows (F1 to exit)");
+    attroff(COLOR_PAIR(4));
+    doupdate();
+
+    top = my_panels[2];
+    while ((ch = getch()) != KEY_F(1))
+    {
+        switch(ch)
+        {
+            case 9:
+                top = (PANEL *)panel_userptr(top);
+                top_panel(top);
+                break;
+        }
+        update_panels();
+        doupdate();
+    }
+    endwin();
+    return 0;
+}
+
+/* Put all the windows */
+void init_wins(WINDOW * wins[], size_t n)
+{
+    int  x, y, i;
+    char label[80];
+
+    y = 2;
+    x = 10;
+
+    for (i = 0; i < n; ++i)
+    {
+        wins[i] = newwin(NLINES, NCOLS, y, x);
+        sprintf(label, "Window Number %d", i + 1);
+        win_show(wins[i], label, i + 1);
+        y += 3;
+        x += 7;
+    }
+}
+
+/* Show the window with a border and a label */
+void win_show(WINDOW * win, char * label, int label_color)
+{
+    int startx, starty, height, width;
+
+    getbegyx(win, starty, startx);
+    getmaxyx(win, height, width);
+
+    box(win, 0, 0);
+    mvwaddch(win, 2, 0, ACS_LTEE);
+    mvwhline(win, 2, 1, ACS_HLINE, width - 2);
+    mvwaddch(win, 2, width - 1, ACS_RTEE);
+
+    print_in_middle(win, 1, 0, width, label, COLOR_PAIR(label_color));
+}
+
+void print_in_middle(WINDOW * win, int starty, int startx, int width, char * string, chtype color)
+{
+    int   length, x, y;
+    float temp;
+
+    if (win == NULL)
+        win = stdscr;
+
+    getyx(win, y, x);
+
+    if (startx != 0)
+        x = startx;
+    if (starty != 0)
+        y = starty;
+    if (width == 0)
+        width = 80;
+
+    length = strlen(string);
+    temp = (width - length) / 2;
+    x = startx + (int)temp;
+    wattron(win, color);
+    mvwprintw(win, y, x, "%s", string);
+    wattroff(win, color);
+    refresh();
 }
